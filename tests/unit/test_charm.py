4367108a25c25aa7e18d67d9d3e9fda2627c3947
Pietro Pasotti <starfire.daemon@gmail.com>
2024-01-08 11:30:26 +0100

    basic setup

---
+++ tests/unit/test_charm.py
@@ -0,0 +1,33 @@
+from unittest.mock import call
+
+import ops
+from scenario import State
+
+
+def test_install_status(ctx, popen_mock):
+    # GIVEN the null state
+    state_in = State()
+
+    # WHEN we get an install event
+    state_out = ctx.run("install", state_in)
+
+    # THEN we go through these statuses
+    assert ctx.unit_status_history == [
+        ops.UnknownStatus(),
+        ops.MaintenanceStatus("installing nginx..."),
+        ops.MaintenanceStatus("setting up ufw rules..."),
+    ]
+
+    # THEN we reach active
+    assert state_out.unit_status == ops.ActiveStatus()
+
+    # THEN we emit the following popen calls
+    popen_mock.assert_called()
+    popen_mock.assert_has_calls(
+        (
+            call(['apt-get', 'update', '-y']),
+            call(['apt-get', 'install', '-y', 'nginx']),
+            call(['ufw', 'allow', 'Nginx HTTP']),
+        ),
+        any_order=True
+    )
